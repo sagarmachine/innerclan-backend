f528b5dc4b0225d56b80c618d26ac1f5a8124350
sagarmachine <sagarmonkey800@gmail.com>
2020-07-24 09:46:53 +0530

    admin authenticatin added

--- src/main/java/com/innerclan/v1/controller/admin/AdminController.java
+++ src/main/java/com/innerclan/v1/controller/admin/AdminController.java
@@ -1,10 +1,15 @@
 package com.innerclan.v1.controller.admin;
 
 
+import com.innerclan.v1.entity.AdminLoginKey;
 import com.innerclan.v1.exception.AuthenticationException;
+import com.innerclan.v1.repository.AdminLoginKeyRepository;
+import com.innerclan.v1.security.JWTUtil;
 import com.innerclan.v1.security.JWTUtilAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -14,10 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/v1/admin")
@@ -30,20 +32,26 @@ public class AdminController {
     String secret;
 
     @Autowired
-    JWTUtilAdmin jwtUtilAdmin;
+    AdminLoginKeyRepository adminLoginKeyRepository;
+
+    @Autowired
+    JWTUtil jwtUtil;
 
     @PostMapping("/authenticate")
     public ResponseEntity<?> authenticateAdmin(@RequestParam("id")String id, @RequestParam("secret")String secret) {
 
         if(this.id.equals(id) && this.secret.equals(secret)){
-            Collection authorities= new ArrayList();
-            //List<SimpleGrantedAuthority> grantedAuthorities = new AdminAuthority().getAuthorities().map(authority -> new SimpleGrantedAuthority(authority)).collect(Collectors.toList()); // (1)
+            AdminLoginKey adminLoginKey = new AdminLoginKey();
+            adminLoginKey.setKey(UUID.randomUUID().toString());
+            adminLoginKeyRepository.save(adminLoginKey);
 
-            authorities.add("ADMIN");
+            User user = new User(id, secret, new ArrayList<>());
+            String jwtToken = jwtUtil.generateToken(user);
+            HttpHeaders headers= new HttpHeaders();
+            headers.add("Authorization","Bearer "+jwtToken);
+            headers.add("Key",adminLoginKey.getKey());
 
-            User user = new User("ADMIN", null, authorities);
-            //String jwt = jwtUtilAdmin
-            return null;
+            return new ResponseEntity<>(headers, HttpStatus.OK);
         }else{
             throw new AuthenticationException("admin's credentials are invalid");
         }
---
+++ src/main/java/com/innerclan/v1/entity/AdminLoginKey.java
@@ -0,0 +1,25 @@
+package com.innerclan.v1.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminLoginKey {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    int id;
+
+    @Column(nullable = false)
+    String key;
+
+}
