4bbff67fd8b621fd18a202063984fbe66174c280
sagarmachine <sagarmonkey800@gmail.com>
2020-07-30 17:51:39 +0530

    security endpoint managed

---
+++ src/main/java/com/innerclan/v1/controller/client/ClientUIController.java
@@ -0,0 +1,24 @@
+package com.innerclan.v1.controller.client;
+
+
+import com.innerclan.v1.repository.UIElementsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/client/ui")
+public class ClientUIController {
+
+    @Autowired
+    UIElementsRepository uiElementsRepository;
+
+    @GetMapping(value="")
+    public ResponseEntity<?> get(){
+        return new ResponseEntity<>(uiElementsRepository.findAll(), HttpStatus.OK);
+    }
+
+}
